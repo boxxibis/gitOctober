087a5b808d8e7c625565270fbaeba13983cce4e5
epena6656 <epena6656@001LAB153010825.neisd.ad>
2015-10-20 10:14:06 -0500

    Fixed it

---
+++ gitOctober/src/October.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class October {
+
+	public static void main(String[] args) {
+		Scanner asdf = new Scanner(System.in);
+		System.out.println("Want to hear a joke about ghosts?");
+		String answer = asdf.nextLine();
+		System.out.println("That's the spirit.");
+		
+	}
+
+}
